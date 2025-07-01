95483ac38910bd72a5ed98adcb181f8896af95e7
nureruu <asanovaiklassnurperi@gmail.com>
2025-07-01 23:03:23 +0600

    hw 4

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -136,3 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# 'DEFAULT_PERMISSION_CLASSES' : [
+#     'rest_framework.permissions.Allow'
+# ]
--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from .views import RegisterView, ConfirmCodeView, CustomTokenView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.authtoken.views import obtain_auth_token
-
+from users .oauth import GoogleLoginView
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('confirm/', ConfirmCodeView.as_view()),
     path('login/', CustomTokenView.as_view()),
     path('refresh/', TokenRefreshView.as_view()),
     path('api/v1/auth-token/', obtain_auth_token, name='api_token_auth'),
+    path('google-login/', GoogleLoginView.as_view()),
     
 ]
